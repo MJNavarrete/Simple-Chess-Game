4a8c3c233e5baa86c6965970d38d41f9c697a6df
asn62 <asn62@txstate.edu>
2024-04-07 21:08:50 -0500

    Added some commetns to the simple game board code

--- SimpleGameBoard.java
+++ SimpleGameBoard.java
@@ -1,23 +1,28 @@
+
+//All of the needed packages for use in the gameboard are here. Some of these are .*, which means that all of the contents of the package are imported but not all of them are used. This however was present in the example code so I'll just leave it
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
-
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+// Class for the game board. Name for the class is the same as the coding example and don't need to change it.
 public class SimpleGameBoard {
 
+    // Private variables that are used in the methods of the function
     private static final int ROWS = 8;
     private static final int COLS = 8;
     private final JPanel[][] gameBoardSquares = new JPanel[ROWS][COLS];
     private JPanel selectedPiece = null;
     private String[][] board = new String[ROWS][COLS];
 
+    // Main function that is used to call an instance of the game board
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new SimpleGameBoard().createAndShowGUI());
     }
 
+    // The main method for setting up the game board, adding the panels to the gameboard JPanel 2d-array and setting it all visible.
     private void createAndShowGUI() {
         JFrame frame = new JFrame("Simple Game Board with Multiple Pieces");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,7 +32,7 @@ public class SimpleGameBoard {
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
                 JPanel panel = new JPanel();
-                panel.setBackground((i + j) % 2 == 0 ? Color.CYAN : Color.WHITE);
+                panel.setBackground((i + j) % 2 == 0 ? Color.CYAN : Color.WHITE); //Change cyan and white to different colors for different board colors
                 panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
                 panel.addMouseListener(new MouseAdapter() {
                     @Override
@@ -46,14 +51,18 @@ public class SimpleGameBoard {
         frame.setVisible(true);
     }
 
+
+    // Method to add all of the piece to the board on top of the panels within the board.
+    // Unicode is there to represent the chess pieces
     private void setupGamePieces() {
+
         //White               King      Queen     wrook    bishop    knight     pawn
         // String[] test = ["\u2654", "\u2655", "\u2656", "\u2657", "\u2658", "\u2659"];
 
         //Black
         // String[] test2 = ["\u265A", "\u265B", "\u265C", "\u265D", "\u265E", "\u265F"];
 
-        
+        // Preset back and front rows for the black and white pieces
         String[] wbackrow = {"\u2656", "\u2658", "\u2657", "\u2654", "\u2655",  "\u2657", "\u2658","\u2656"};
         String[] wfrontrow = {"\u2659", "\u2659", "\u2659", "\u2659","\u2659", "\u2659", "\u2659", "\u2659"};
 
@@ -61,10 +70,10 @@ public class SimpleGameBoard {
         String[] bfrontrow = {"\u265F","\u265F","\u265F","\u265F","\u265F","\u265F","\u265F","\u265F"};
 
 
+        // For loop that will iterate through the rows and each time it hits a significant row value, it will stop and loop through each col and replace the section there with unicode for a game piece
+        for(int i = 0; i < ROWS; i++){
 
-        for(int i = 0; i < COLS; i++){
-
-
+            // Signifies the first row of the board in terms of the array, which is the furthest row form the white player
             if(i == 0){
                 for(int j = 0; j < bbackrow.length; j++){
                 board[i][j] = bbackrow[j];
@@ -79,6 +88,7 @@ public class SimpleGameBoard {
                 }
             }
 
+            // Signifies the front row of the black player side 
             else if(i == 1){
                 for(int j = 0; j < bfrontrow.length; j++){
                     board[i][j] = bfrontrow[j];
@@ -92,6 +102,7 @@ public class SimpleGameBoard {
                 }       
             }
 
+            // Signifies the front row for the white player
             else if(i == 6){
                 for(int j = 0; j < wfrontrow.length; j++){
                     board[i][j] = wfrontrow[j];
@@ -105,6 +116,7 @@ public class SimpleGameBoard {
                 }       
             }
 
+            // Signifies the back row for the white player
             else if(i == 7){
                 for(int j = 0; j < wbackrow.length; j++){
                     board[i][j] = wbackrow[j];
@@ -118,6 +130,7 @@ public class SimpleGameBoard {
                 }       
             }
 
+            // Sets the borders of the blank spaces to red for the handleMouseClick method
             else {
                 for(int j = 0; j < wbackrow.length; j++){
                     gameBoardSquares[i][j].setBorder(BorderFactory.createLineBorder(Color.RED));
@@ -125,56 +138,40 @@ public class SimpleGameBoard {
                 }
 
             }
-
-
-            // for(int x = 0; x < ROWS; x++)
-            // {
-            //     gameBoardSquares[i][x].setBorder(BorderFactory.createLineBorder(Color.RED));
-
-            // }
-
+            //sets the other rows in the board to black "" Strings
             board[i] = new String[] {"", "" , "", "", "", "" , "", ""};
 
         }
     }
 
+    // Handle mouse click method to handle when the user clicks on the board.
     private void handleMouseClick(JPanel clickedPanel) {
         Color pieceColor = ((LineBorder)clickedPanel.getBorder()).getLineColor();
 
+        // If the border that the user clicks on in not one of the borders representing the piece colors and the space is null, then it doesn't allow the move.
         if(pieceColor == Color.RED && selectedPiece == null){
             return;
 
         }
         
-            if(selectedPiece == null){
-                clickedPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
-                selectedPiece = clickedPanel;
+        // if the selected piece is null, then we set the border of the selected square to blue to signify selection and then we set the selected piece to the clicked panel
+        if(selectedPiece == null){
+            clickedPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+            selectedPiece = clickedPanel;
                 
-            } else{
-                movePiece(selectedPiece, clickedPanel);
-                selectedPiece = null;
+        // If the selected piece is already selected, then we call the move piece method and set the selected piece back to null for future use.
+        } else{
+            movePiece(selectedPiece, clickedPanel);
+            selectedPiece = null;
 
-            }
+        }
             
-        
-
-            return;
+        return;
        
 
     }
 
-    // private int[] findPanelPosition(JPanel panel) {
-    //     for (int i = 0; i < ROWS; i++) {
-    //         for (int j = 0; j < COLS; j++) {
-    //             if (gameBoardSquares[i][j] == panel) {
-    //                 return new int[]{i, j};
-    //             }
-    //         }
-    //     }
-    //     return new int[]{-1, -1}; // Not found
-    // }
-
-
+    // Method to actually mvoe the unicode pieces on the board. Takes in a from and a to panel so that we can remove everything from the 'from' panel and move it to the 'to' panel
     private void movePiece(JPanel from, JPanel to) {
         JLabel piece = (JLabel) from.getComponent(0);
         from.setBorder(BorderFactory.createLineBorder(Color.RED));
